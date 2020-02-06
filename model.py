9d8286f6769317d4661976e95320dbaa62b398af
MKemalYildirim <mehmet.kemal111@gmail.com>
2020-02-06 21:28:17 +0300

    update

---
+++ model.py
@@ -0,0 +1,43 @@
+#!flask/bin/python
+from flask import Flask, jsonify, abort, request, make_response, url_for
+from flask_httpauth import HTTPBasicAuth
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import session
+from flask import g
+from passlib.apps import custom_app_context as pwd_context
+from itsdangerous import (TimedJSONWebSignatureSerializer
+                          as Serializer, BadSignature, SignatureExpired)
+
+app = Flask(__name__)
+
+db = SQLAlchemy(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/Mehmet_Kemal/Desktop/web_services/veriler.db'
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(32), index = True)
+    password_hash = db.Column(db.String(128))
+
+    def hash_password(self, password):
+        self.password_hash = pwd_context.encrypt(password)
+
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
+    def generate_auth_token(self, expiration = 600):
+        s = Serializer(app.config['SECRET_KEY'], expires_in = expiration)
+        return s.dumps({ 'id': self.id })
+
+    @staticmethod
+    def verify_auth_token(token):
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except SignatureExpired:
+            return None # valid token, but expired
+        except BadSignature:
+            return None # invalid token
+        user = User.query.get(data['id'])
+        return user
--- rest-server.py
+++ rest-server.py
@@ -8,7 +8,7 @@ from passlib.apps import custom_app_context as pwd_context
 from itsdangerous import (TimedJSONWebSignatureSerializer
                           as Serializer, BadSignature, SignatureExpired)
 
-
+import model
 
 
 app = Flask(__name__)
@@ -17,32 +17,6 @@ db = SQLAlchemy(app)
 auth = HTTPBasicAuth()
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/Mehmet_Kemal/Desktop/web_services/veriler.db'
 
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(32), index = True)
-    password_hash = db.Column(db.String(128))
-
-    def hash_password(self, password):
-        self.password_hash = pwd_context.encrypt(password)
-
-    def verify_password(self, password):
-        return pwd_context.verify(password, self.password_hash)
-    def generate_auth_token(self, expiration = 600):
-        s = Serializer(app.config['SECRET_KEY'], expires_in = expiration)
-        return s.dumps({ 'id': self.id })
-
-    @staticmethod
-    def verify_auth_token(token):
-        s = Serializer(app.config['SECRET_KEY'])
-        try:
-            data = s.loads(token)
-        except SignatureExpired:
-            return None # valid token, but expired
-        except BadSignature:
-            return None # invalid token
-        user = User.query.get(data['id'])
-        return user
 
 @app.route('/api/token')
 @auth.login_required
@@ -53,10 +27,10 @@ def get_auth_token():
 @auth.verify_password
 def verify_password(username_or_token, password):
     # first try to authenticate by token
-    user = User.verify_auth_token(username_or_token)
+    user = model.User.verify_auth_token(username_or_token)
     if not user:
         # try to authenticate with username/password
-        user = User.query.filter_by(username = username_or_token).first()
+        user = model.User.query.filter_by(username = username_or_token).first()
         if not user or not user.verify_password(password):
             return False
     g.user = user
@@ -68,12 +42,12 @@ def new_user():
     password = request.json.get('password')
     if username is None or password is None:
         abort(400) # missing arguments
-    if User.query.filter_by(username = username).first() is not None:
+    if model.User.query.filter_by(username = username).first() is not None:
         abort(400) # existing user
-    user = User(username = username)
+    user = model.User(username = username)
     user.hash_password(password)
-    db.session.add(user)
-    db.session.commit()
+    model.db.session.add(user)
+    model.db.session.commit()
     return jsonify({ 'username': user.username })
 
 
@@ -81,13 +55,20 @@ def new_user():
 @app.route("/delete/<string:name>")
 @auth.login_required
 def DELETE(name):
-    try:
-     user = User.query.filter_by(username=name).first()
-     db.session.delete(user)
-     db.session.commit()
-     return "Silme başarılı"
-    except:
-      return "Kişi Bulunamadı"
+    user = model.User.query.filter_by(username=name).first()
+    if user is None:
+        return "Böyle Bir Kişi Yok"
+    else:
+      try:  
+        model.db.session.delete(user)
+        model.db.session.commit()
+        return "Silme başarılı"
+    
+      except:
+        
+        return "Kişi silinemedi"
+    
+      
 
 
 
@@ -99,7 +80,7 @@ def unauthorized():
 def not_found(error):
     return make_response(jsonify( { 'error': 'Not found' } ), 404)
 
-db.create_all()
+model.db.create_all()
 
 if __name__ == '__main__':
     app.run(debug = True)
