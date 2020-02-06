64d8f40c717b116584a6ded8a0dd351c38e39714
MKemalYildirim <mehmet.kemal111@gmail.com>
2020-02-06 21:07:33 +0300

    a

--- rest-server.py
+++ rest-server.py
@@ -3,57 +3,103 @@ from flask import Flask, jsonify, abort, request, make_response, url_for
 from flask_httpauth import HTTPBasicAuth
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import session
-import models
+from flask import g
+from passlib.apps import custom_app_context as pwd_context
+from itsdangerous import (TimedJSONWebSignatureSerializer
+                          as Serializer, BadSignature, SignatureExpired)
+
+
 
 
 app = Flask(__name__)
 
+db = SQLAlchemy(app)
 auth = HTTPBasicAuth()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/Mehmet_Kemal/Desktop/web_services/veriler.db'
 
-@auth.get_password
-def get_password(name):
-        user =models.User.query.filter_by(username=name).first()
-        if name == user.username:
-            return user.password
-        return None
-    
-@auth.error_handler
-def unauthorized():
-    return make_response(jsonify( { 'error': 'Unauthorized access' } ), 401) 
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(32), index = True)
+    password_hash = db.Column(db.String(128))
 
-@app.errorhandler(404)
-def not_found(error):
-    return make_response(jsonify( { 'error': 'Not found' } ), 404)
+    def hash_password(self, password):
+        self.password_hash = pwd_context.encrypt(password)
 
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
+    def generate_auth_token(self, expiration = 600):
+        s = Serializer(app.config['SECRET_KEY'], expires_in = expiration)
+        return s.dumps({ 'id': self.id })
 
-@app.route("/add", methods=['POST'])
-def post():
+    @staticmethod
+    def verify_auth_token(token):
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except SignatureExpired:
+            return None # valid token, but expired
+        except BadSignature:
+            return None # invalid token
+        user = User.query.get(data['id'])
+        return user
 
-    content = request.get_json()
-    name=content['name']
-    pas=content['pass']
-    try:
-     new_user=models.User(username=name,password=pas)
-     models.db.session.add(new_user)
-     models.db.session.commit()
-     return "Ekleme Başarılı"
-    except:
-     return "Ekleme Başarısız"
+@app.route('/api/token')
+@auth.login_required
+def get_auth_token():
+    token = g.user.generate_auth_token()
+    return jsonify({ 'token': token.decode('ascii') })
+
+@auth.verify_password
+def verify_password(username_or_token, password):
+    # first try to authenticate by token
+    user = User.verify_auth_token(username_or_token)
+    if not user:
+        # try to authenticate with username/password
+        user = User.query.filter_by(username = username_or_token).first()
+        if not user or not user.verify_password(password):
+            return False
+    g.user = user
+    return True
+
+@app.route('/api/users', methods = ['POST'])
+def new_user():
+    username = request.json.get('username')
+    password = request.json.get('password')
+    if username is None or password is None:
+        abort(400) # missing arguments
+    if User.query.filter_by(username = username).first() is not None:
+        abort(400) # existing user
+    user = User(username = username)
+    user.hash_password(password)
+    db.session.add(user)
+    db.session.commit()
+    return jsonify({ 'username': user.username })
+
 
 
 @app.route("/delete/<string:name>")
 @auth.login_required
 def DELETE(name):
     try:
-     user = models.User.query.filter_by(username=name).first()
-     models.db.session.delete(user)
-     models.db.session.commit()
+     user = User.query.filter_by(username=name).first()
+     db.session.delete(user)
+     db.session.commit()
      return "Silme başarılı"
     except:
       return "Kişi Bulunamadı"
 
 
 
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify( { 'error': 'Unauthorized access' } ), 401)
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify( { 'error': 'Not found' } ), 404)
+
+db.create_all()
 
 if __name__ == '__main__':
     app.run(debug = True)
